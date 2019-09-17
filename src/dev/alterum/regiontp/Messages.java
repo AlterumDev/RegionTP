759ac0a357b794d3c6a3b20cc8e0de082c1017aa
Evan <justgamerimportant@gmail.com>
2019-09-17 09:42:49 -0700

    Permission reading updates

--- src/dev/alterum/regiontp/Messages.java
+++ src/dev/alterum/regiontp/Messages.java
@@ -4,9 +4,11 @@ public class Messages {
 	public static String prefix;
 	public static String usage;
 	public static String no_regions_found;
-	public static String region_not_found;
+	public static String origin_region_not_found;
+	public static String dest_region_not_found;
+	public static String region_lacks_tp_point;
+	public static String none_found_in_region;
 	public static String tp_successful;
-	public static String none_in_region;
 	public static String player_forced_tp;
 	public static String plugin_reloaded;
 	public static String missing_permission;
