fc707f6214277f070d5ee2cd57e4db03624b3f49
Evan <justgamerimportant@gmail.com>
2019-09-17 09:28:18 -0700

    Main class improvements for configuration & cleanup

--- src/dev/alterum/regiontp/RegionTP.java
+++ src/dev/alterum/regiontp/RegionTP.java
@@ -1,61 +1,37 @@
 package dev.alterum.regiontp;
 
-import java.util.List;
-import java.util.UUID;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.bringholm.nametagchanger.NameTagChanger;
-import com.hazebyte.crate.api.crate.Crate;
-import com.hazebyte.crate.api.crate.CrateType;
-import com.hazebyte.crate.api.event.CrateInteractEvent;
-
 import dev.alterum.regiontp.Messages;
-import net.haoshoku.nick.NickPlugin;
 
 public class RegionTP extends JavaPlugin implements Listener {
 
 	private File configfile;
 	public FileConfiguration configdata;
-	private String version = "0.4";
+	public String version = "0.5.2";
 
 	String prefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("prefix") + " ");
 
 	public void onEnable() {
 		createFiles();
 		getServer().getPluginManager().registerEvents(this, this);
-		getCommand("spawnall").setExecutor(new RegionTPCommand(this));
+		getCommand("regiontp").setExecutor(new RegionTPCommand(this));
 		loadMessages();
-
-	
 	}
 
 	public void onDisable() {
@@ -90,10 +66,12 @@ public class RegionTP extends JavaPlugin implements Listener {
 		Messages.prefix = getConfigFile().getString("prefix");
 		Messages.usage = getConfigFile().getString("usage");
 		Messages.no_regions_found = getConfigFile().getString("no-regions-found");
-		Messages.region_not_found = getConfigFile().getString("region-not-found");
+		Messages.origin_region_not_found = getConfigFile().getString("origin-region-not-found");
+		Messages.dest_region_not_found = getConfigFile().getString("dest-region-not-found");
+		Messages.region_lacks_point = getConfigFile().getString("region-lacks-tp-point");
+		Messages.none_in_region = getConfigFile().getString("none-found-in-origin");
 		Messages.tp_successful = getConfigFile().getString("tp-successful");
-		Messages.none_in_region = getConfigFile().getString("none-in-region");
-		Messages.player_forced_tp = getConfigFile().getString("player-forced-tp");
+		Messages.player_forced_tp = getConfigFile().getString("player-tp-message");
 		Messages.plugin_reloaded = getConfigFile().getString("plugin-reloaded");
 		Messages.missing_permission = getConfigFile().getString("missing-permission");
 		Messages.command_permission = getConfigFile().getString("command-permission");
@@ -108,7 +86,7 @@ public class RegionTP extends JavaPlugin implements Listener {
 		if (cmd.getName().equalsIgnoreCase("rtpreload")) {
 			  if (!(sender instanceof org.bukkit.entity.Player)) {
 			    try {
-			      this.configdata.load(this.configfile);
+			      this.configData.load(this.configfile);
 			    } catch (FileNotFoundException e) {
 			      e.printStackTrace();
 			    } catch (IOException e) {
