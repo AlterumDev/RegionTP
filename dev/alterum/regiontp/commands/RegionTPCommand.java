611607f5211beee9f32f412b2b153c3e5fd37d1a
Evan <justgamerimportant@gmail.com>
2019-09-18 11:36:52 -0700

    Revamp RegionTP Command for new arguments
    Includes:
    - /regiontp (region) (x) (y) (z)
    - /regiontp (region) (region) - teleportation point

--- dev/alterum/regiontp/commands/RegionTPCommand.java
+++ dev/alterum/regiontp/commands/RegionTPCommand.java
@@ -12,7 +12,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-import com.google.common.base.Joiner;
 import com.sk89q.worldguard.bukkit.WGBukkit;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 
@@ -25,7 +24,7 @@ public class RegionTPCommand implements CommandExecutor {
 	private final RegionTP plugin;
 	private final FileConfiguration config;
 	private int playersTotal = 0;
-	private String prefix = ChatColor.translateAlternateColorCodes('&', Messages.prefix);
+	private String prefix = Utils.format(Messages.prefix);
 
 	public RegionTPCommand(RegionTP plugin) {
 		this.plugin = plugin;
@@ -85,51 +84,59 @@ public class RegionTPCommand implements CommandExecutor {
 			}
 		}
 
-		
+		// Will revamp how this works in the future to handle simple structure without multiple WorldGuard check methods.
 		if (player.hasPermission(Messages.tp_permission) || !player.hasPermission(Messages.admin_permission)) {
 			if (args == null || args.length < 1 || args.length == 0) {
-				player.sendMessage(Messages.prefix + ChatColor.translateAlternateColorCodes('&', Messages.regiontp_usage));
+				player.sendMessage(Utils.format(Messages.regiontp_usage.replace("{PREFIX}", prefix));
 			} else {
-				playersTotal = 0;
-				String cmdArgs = Joiner.on(' ').join(args);
-				RegionManager regions = WGBukkit.getRegionManager((org.bukkit.World) player.getWorld());
+				if(args.length > 2) {
+					if(!args.length == 4) 
+						player.sendMessage(Utils.format(Messages.regiontp_usage.replace("{PREFIX}, prefix));
+					else {
+						Location destLoc = new Location(player.getWorld(), args[1], args[2], args[3]);
+						// Destination teleportation code here.
+					}
+				} else {
+					playersTotal = 0;
+					String cmdArgs = String.join(' ', args);
+					RegionManager regions = WGBukkit.getRegionManager((org.bukkit.World) player.getWorld());
 
-				if (regions != null) {
-					if (regions.hasRegion(cmdArgs)) {
-						Bukkit.getServer().getScheduler().runTask(plugin, new Runnable() {
-							@Override
-							public void run() {
-								for (Player serverPlayer : Bukkit.getServer().getOnlinePlayers()) {
-									if (!serverPlayer.hasPermission(Messages.bypass_permission) || !player.hasPermission(Messages.admin_permission)) {
-										if (serverPlayer.getWorld().equals(player.getWorld())) {
-											if (regions.getRegion(cmdArgs).contains(
-													(int) serverPlayer.getLocation().getX(),
-													(int) serverPlayer.getLocation().getY(),
-													(int) serverPlayer.getLocation().getZ())) {
-												serverPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&',
-														Messages.player_teleported));
-												Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),
-														"cmi spawn " + serverPlayer.getName());
-												playersTotal = playersTotal + 1;
+					if (regions != null) {
+						if (regions.hasRegion(cmdArgs)) {
+							Bukkit.getServer().getScheduler().runTask(plugin, new Runnable() {
+								@Override
+								public void run() {
+									for (Player sPlayer : Bukkit.getServer().getOnlinePlayers()) {
+										if (!sPlayer.hasPermission(Messages.bypass_permission) || !sPlayer.hasPermission(Messages.admin_permission)) {
+											if (sPlayer.getWorld().equals(player.getWorld())) {
+												if (regions.getRegion(cmdArgs).contains(
+														(int) sPlayer.getLocation().getX(),
+														(int) sPlayer.getLocation().getY(),
+														(int) sPlayer.getLocation().getZ())) {
+													sPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&',
+															Messages.player_teleported));
+													// Region teleportation point code here.
+													playersTotal = playersTotal + 1;
+												}
 											}
 										}
 									}
+
+									if (playersTotal > 0) {
+										player.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&',
+												Messages.tp_success.replace("{REGION}", cmdArgs)));
+									} else {
+										player.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&',
+												Messages.none_in_region.replace("{REGION}", cmdArgs)));
+									}
 								}
-								
-								if (playersTotal > 0) {
-									player.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&',
-											Messages.tp_success.replace("{REGION}", cmdArgs)));
-								} else {
-									player.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&',
-											Messages.none_in_region.replace("{REGION}", cmdArgs)));
-								}
-							}
-						});
+							});
+						} else {
+							player.sendMessage(prefix + Utils.format(Messages.no_origin_region.replace("{PREFIX}", prefix)));
+						}
 					} else {
-						player.sendMessage(prefix + Utils.format(Messages.no_origin_region.replace("{PREFIX}", prefix)));
+						player.sendMessage(prefix + Utils.format(Messages.no_regions_found.replace("{PREFIX}", prefix)));
 					}
-				} else {
-					player.sendMessage(prefix + Utils.format(Messages.no_regions_found.replace("{PREFIX}", prefix)));
 				}
 			}
 		} else {
