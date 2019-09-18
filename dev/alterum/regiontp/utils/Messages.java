092918dac8614ab4fec0e6e610bdeb8fc5462109
JustGamer <justgamerimportant@gmail.com>
2019-09-17 21:57:29 -0700

    Configuration msgs & perms revamp
    Permission layout included - possible final draft.
    Messages layout - may redesign in the future.

---
+++ dev/alterum/regiontp/utils/Messages.java
@@ -0,0 +1,29 @@
+package dev.alterum.regiontp.utils;
+
+public class Messages {
+	public static String prefix;
+	
+	public static String regiontp_usage;
+	public static String tpcoords_usage;
+	public static String spawntp_usage;
+
+	public static String no_regions_found;
+	public static String no_origin_region;
+	public static String no_dest_region;
+	public static String region_lacks_point;
+	public static String none_in_region;
+	public static String spawn_not_set;
+	public static String tp_success;
+	public static String player_teleported;
+	public static String spawn_set_success;
+	public static String plugin_reloaded;
+	public static String missing_permission;
+	
+	public static String tp_permission;
+	public static String tpcoords_permission;
+	public static String spawntp_permission;
+	public static String bypass_permission;
+	public static String setspawn_permission;
+	public static String reload_permission;
+	public static String admin_permission;
+}
