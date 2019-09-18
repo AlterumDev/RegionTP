1226851814236754e3616983b9550b476e9a634f
JustGamer <justgamerimportant@gmail.com>
2019-09-18 02:43:56 -0700

    Main class configuration method clean up

--- dev/alterum/regiontp/RegionTP.java
+++ dev/alterum/regiontp/RegionTP.java
@@ -52,12 +52,12 @@ public class RegionTP extends JavaPlugin implements Listener {
 		}
 	}
 
-	public FileConfiguration getConfigFile() {
-		return this.configdata;
+	public File getConfigFile() {
+		return this.configfile;
 	}
 
 	public void loadMessages() {
-		FileConfiguration config = getConfigFile();
+		FileConfiguration config = getConfig();
 		
 		Messages.prefix = config.getString("prefix");
 		
@@ -70,18 +70,18 @@ public class RegionTP extends JavaPlugin implements Listener {
 		Messages.no_dest_region = config.getString("dest-region-not-found");
 		Messages.region_lacks_point = config.getString("region-lacks-tp-point");
 		Messages.none_in_region = config.getString("none-found-in-origin");
-		Messages.tp_success = getConfigFile().getString("tp-successful");
+		Messages.tp_success = config.getString("tp-successful");
 		Messages.player_teleported = config.getString("player-tp-message");
 		Messages.spawn_set_success = config.getString("spawn-set-success");
 		Messages.plugin_reloaded = config.getString("plugin-reloaded");
 		Messages.missing_permission = config.getString("missing-permission");
 		
-		Messages.tp_permission = getConfigFile().getString("regiontp-permission");
-		Messages.tpcoords_permission = getConfigFile().getString("tpcoords-permission");
-		Messages.spawntp_permission = getConfigFile().getString("spawntp-permission");
-		Messages.bypass_permission = getConfigFile().getString("bypass-permission");
-		Messages.setspawn_permission = getConfigFile().getString("setspawn-permission");
-		Messages.reload_permission = getConfigFile().getString("reload-permission");
-		Messages.admin_permission = getConfigFile().getString("admin-permission");
+		Messages.tp_permission = config.getString("regiontp-permission");
+		Messages.tpcoords_permission = config.getString("tpcoords-permission");
+		Messages.spawntp_permission = config.getString("spawntp-permission");
+		Messages.bypass_permission = config.getString("bypass-permission");
+		Messages.setspawn_permission = config.getString("setspawn-permission");
+		Messages.reload_permission = config.getString("reload-permission");
+		Messages.admin_permission = config.getString("admin-permission");
 	}
 }
